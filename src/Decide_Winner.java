fe259ace5e7f7b4e4c1b80ea8a9090db2e7fa69b
Cobin-Black <halfkey.studio.games@gmail.com>
2025-03-08 18:13:14 -0500

    Project Setup
    Make the repository and getting the layout down

---
+++ src/Decide_Winner.java
@@ -0,0 +1,3 @@
+public class Decide_Winner {
+
+}
---
+++ src/Game.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Game {
+    public static void main(String[] args) throws Exception {
+        Scanner scnr = new Scanner(System.in);
+        char[] board_spots = {'0', '1', '2', '3', '4', '5', '6', '7', '8'};
+
+        System.out.print(board_spots[0] + " | " + board_spots[1] + " | " + board_spots[2] + "\n" +
+                         "--|---|--\n" +
+                         board_spots[3] + " | " + board_spots[4] + " | " + board_spots[5] + "\n" +
+                         "--|---|--\n" +
+                         board_spots[6] + " | " + board_spots[7] + " | " + board_spots[8]);
+    }
+}
