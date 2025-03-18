01b50ab595395c12d901f965c2dc0e7aea9771a8
Cobin-Black <halfkey.studio.games@gmail.com>
2025-03-18 18:51:47 -0400

    Starting the Game (Finishing Touches)
    Just establishing the which player starts

--- src/Game.java
+++ src/Game.java
@@ -11,18 +11,29 @@ public class Game {
         int winner = 0;
 
         int coin_flip = rand.nextInt(2);
-        System.out.println("Heads is Player 1 and Tails is Player 2");
-        System.out.println("\nCoin flip result: " + (coin_flip == 0 ? "Heads (Player 1)" : "Tails (Player 2)"));
+        System.out.println("Heads is Player 1:(X) and Tails is Player 2:(O)");
+        System.out.println("\nCoin flip result: " + (coin_flip == 0 ? "Heads (Player 1) starts" : "Tails (Player 2) starts"));
 
-        while(winner == 0)
-        {
+        int current_player = coin_flip;
 
-        }
+        System.out.print(current_player);
 
-        System.out.print(board_spots[0] + " | " + board_spots[1] + " | " + board_spots[2] + "\n" +
-                         "--|---|--\n" +
-                         board_spots[3] + " | " + board_spots[4] + " | " + board_spots[5] + "\n" +
-                         "--|---|--\n" +
-                         board_spots[6] + " | " + board_spots[7] + " | " + board_spots[8]);
+        // while(winner == 0)
+        // {
+        //     System.out.println(board_spots[0] + " | " + board_spots[1] + " | " + board_spots[2] + "\n" +
+        //                  "--|---|--\n" +
+        //                  board_spots[3] + " | " + board_spots[4] + " | " + board_spots[5] + "\n" +
+        //                  "--|---|--\n" +
+        //                  board_spots[6] + " | " + board_spots[7] + " | " + board_spots[8]);
+
+            
+        // }
+
+    //     System.out.print(board_spots[0] + " | " + board_spots[1] + " | " + board_spots[2] + "\n" +
+    //                      "--|---|--\n" +
+    //                      board_spots[3] + " | " + board_spots[4] + " | " + board_spots[5] + "\n" +
+    //                      "--|---|--\n" +
+    //                      board_spots[6] + " | " + board_spots[7] + " | " + board_spots[8]);
+    // }
     }
 }
